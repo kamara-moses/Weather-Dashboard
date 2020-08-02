3852764bc617a93eb521aab1c792546a088c7d44
Moses Kamara <kamara.moses@yahoo.com>
2020-08-02 14:57:18 -0700

    added some finishing touches

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-var city = $('.search-field').val();
-var cities = [];
+var city = $('.search-field').val().trim();
 
 var APIKey = '7439220f89767ecc92468da6aaab2380'
 
-$('.searchBtn').on('click', function () {
+$('.searchBtn').on('click', function (event) {
     var currentDate = moment().format('M/DD/YYYY');
     city = $('.search-field').val();
-    $('.search-field').val('');
+    //newCities.unShift(city);
 
+    var newCities = JSON.parse(localStorage.getItem('savedCities'));
+    if (newCities != null) {
+        for (var i = 0; i < newCities.length; i++) {
+            if (newCities.indexOf(city) === -1) {
+                newCities.push(city);
+            }
+        }
+    } else {
+        newCities = [city];
+    }
+    localStorage.setItem('savedCities', JSON.stringify(newCities));
 
+    rendersavedCities();
     var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
         + city + '&units=imperial' + '&appid=' + APIKey;
 
@@ -19,7 +30,6 @@ $('.searchBtn').on('click', function () {
     })
         .then(function (response) {
             var icon = 'https://api.openweathermap.org/img/w/' + response.weather[0].icon + '.png';
-            console.log(response);
             $('.city-name').html('<h1>' + response.name + '<img src=' + icon + '>' + '</h1>');
             $('.currentDate').text(moment().format('M/DD/YYYY'));
             $('#temp').text('Temperature: ' + response.main.temp + '°F');
@@ -48,11 +58,7 @@ $('.searchBtn').on('click', function () {
                 $('#uv-index').text('Uv-Index: ' + uvIndex);
                 $('#uv-index').attr('style', 'background-color: ' + uvColor);
             });
-            var cities = JSON.parse(localStorage.getItem(cities)) || [];
-            cities.push(city);
-            var newCities = JSON.stringify(cities);
-            console.log(newCities);
-            localStorage.setItem('cities', newCities);
+
         })
 
     var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey;
@@ -64,7 +70,6 @@ $('.searchBtn').on('click', function () {
         medthod: 'GET'
     })
         .then(function (response) {
-            console.log(response);
             var icon1 = 'http://openweathermap.org/img/w/' + response.list[1].weather[0].icon + '.png';
             var icon2 = 'http://openweathermap.org/img/w/' + response.list[9].weather[0].icon + '.png';
             var icon3 = 'http://openweathermap.org/img/w/' + response.list[17].weather[0].icon + '.png';
@@ -78,31 +83,52 @@ $('.searchBtn').on('click', function () {
             var date5 = response.list[33].dt_txt;
 
             $('#first #date1').html('<h5>' + moment(date1).format('M/DD/YYYY') + '</h5>');
-            console.log(date1);
-            $('#first #icon1').html('<img src=' + icon1 + '>')
-            $('#first #temp1').text('Temp: ' + ((response.list[1].main.temp_max - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
+            $('#first #icon1').html('<img src=' + icon1 + '>');
+            $('#first #temp1').text('Temp: ' + ((response.list[1].main.temp - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
             $('#first #humidity1').text('Humidity: ' + response.list[1].main.humidity);
 
             $('#second #date2').html('<h5>' + moment(date2).format('M/DD/YYYY') + '</h5>');
-            $('#second #icon2').html('<img src=' + icon2 + '>')
-            $('#second #temp2').text('Temp: ' + ((response.list[9].main.temp_max - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
+            $('#second #icon2').html('<img src=' + icon2 + '>');
+            $('#second #temp2').text('Temp: ' + ((response.list[9].main.temp - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
             $('#second #humidity2').text('Humidity: ' + response.list[9].main.humidity);
 
             $('#third #date3').html('<h5>' + moment(date3).format('M/DD/YYYY') + '</h5>');
-            $('#third #icon3').html('<img src=' + icon3 + '>')
-            $('#third #temp3').text('Temp: ' + ((response.list[17].main.temp_max - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
+            $('#third #icon3').html('<img src=' + icon3 + '>');
+            $('#third #temp3').text('Temp: ' + ((response.list[17].main.temp - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
             $('#third #humidity3').text('Humidity: ' + response.list[17].main.humidity);
 
             $('#fourth #date4').html('<h5>' + moment(date4).format('M/DD/YYYY') + '</h5>');
-            $('#fourth #icon4').html('<img src=' + icon4 + '>')
-            $('#fourth #temp4').text('Temp: ' + ((response.list[25].main.temp_max - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
+            $('#fourth #icon4').html('<img src=' + icon4 + '>');
+            $('#fourth #temp4').text('Temp: ' + ((response.list[25].main.temp - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
             $('#fourth #humidity4').text('Humidity: ' + response.list[25].main.humidity);
 
             $('#fifth #date5').html('<h5>' + moment(date5).format('M/DD/YYYY') + '</h5>');
-            $('#fifth #icon5').html('<img src=' + icon5 + '>')
-            $('#fifth #temp5').text('Temp: ' + ((response.list[33].main.temp_max - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
+            $('#fifth #icon5').html('<img src=' + icon5 + '>');
+            $('#fifth #temp5').text('Temp: ' + ((response.list[33].main.temp - 273.15) * 1.8 + 32).toFixed(2) + ' °F');
             $('#fifth #humidity5').text('Humidity: ' + response.list[33].main.humidity);
 
         })
+    function rendersavedCities() {
+        $('.searchedCities').empty();
+        while (newCities.length > 5) {
+            newCities.splice(-1, 1)
+        }
+        for (var i = 0; i < newCities.length; i++) {
+            listItem = $('<li>');
+            listItem.attr('class', 'list-group-item')
+            listItem.text(newCities[i])
+            $('.searchedCities').append(listItem)
+            console.log(newCities[i])
+        }
+    }
+    $('.searchedCities').on('click', 'list-group-item', function(event) {
+        event.preventDefault();
+        city = savedCities;
+    });
+    $('.clearBtn').on('click', function () {
+        $('.searchedCities').remove();
+        localStorage.clear();
+    })
 })
+
 
